6ad72571db7231292f402bed6c48be849938271d
vikasvaish21 <53097544+vikasvaish21@users.noreply.github.com>
2019-07-20 19:00:21 +0530

    Add files via upload

---
+++ project2/project2/meme.swift
@@ -0,0 +1,18 @@
+//
+//  meme.swift
+//  project2
+//
+//  Created by vikas on 23/06/19.
+//  Copyright © 2019 project1. All rights reserved.
+//
+
+import UIKit
+
+struct meme
+{
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
+
